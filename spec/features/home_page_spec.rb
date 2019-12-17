38c9fba19efff87d40740abd2f8b7aa7aead8a19
Zhabskyi <poltavagaz@gmail.com>
2019-12-17 02:07:31 +0000

    Fix broken image done

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -14,6 +14,15 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
         price: 64.99
       )
     end
+
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+    
   end
 
   scenario "They see all products" do
